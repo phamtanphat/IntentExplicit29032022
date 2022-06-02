f1553804b4f52dfc672ce80a60ce5027997061e8
Phat <phatdroid94@gmail.com>
2022-06-02 18:17:34 +0700

    get data from name

--- app/src/main/java/com/example/intentexplicit29032022/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit29032022/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.intentexplicit29032022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] arrNameImages;
+    ImageView imgRandom, imgPick;
+    Random random;
+    int valueImgRandom = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgPick = findViewById(R.id.imgPick);
+        imgRandom = findViewById(R.id.imgRandom);
+        random = new Random();
+        arrNameImages = getResources().getStringArray(R.array.arr_images);
+        int indexRandom = random.nextInt(arrNameImages.length);
+        valueImgRandom = getResources().getIdentifier(arrNameImages[indexRandom],"drawable",getPackageName());
+        imgRandom.setImageResource(valueImgRandom);
+
     }
 }
